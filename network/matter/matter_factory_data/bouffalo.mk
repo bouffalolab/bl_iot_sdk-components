0fb2dfd1f28c4e62497ec9406509f7f612052e47
wyhong <wyhong@bouffalolab.com>
2023-09-21 20:02:03 +0800

    add matter factory data code

--- network/matter/matter_factory_data/bouffalo.mk
+++ network/matter/matter_factory_data/bouffalo.mk
@@ -1,6 +1,25 @@
-COMPONENT_LIB_ONLY := 1
-COMPONENT_ADD_INCLUDEDIRS += include
-LIBS ?= matter_factory_data
-COMPONENT_ADD_LDFLAGS += -L$(COMPONENT_PATH)/lib $(addprefix -l,$(LIBS))
-ALL_LIB_FILES := $(patsubst %,$(COMPONENT_PATH)/lib/lib%.a,$(LIBS))
-COMPONENT_ADD_LINKER_DEPS := $(ALL_LIB_FILES)
+# Component Makefile
+#
+
+## These include paths would be exported to project level
+COMPONENT_ADD_INCLUDEDIRS    += include
+
+## not be exported to project level
+COMPONENT_PRIV_INCLUDEDIRS :=
+
+## This component's src 
+COMPONENT_SRCS := efuse_slot.c \
+                  matter_factory_data.c
+
+COMPONENT_OBJS := $(patsubst %.cpp,%.o, $(filter %.cpp,$(COMPONENT_SRCS))) $(patsubst %.c,%.o, $(filter %.c,$(COMPONENT_SRCS))) $(patsubst %.S,%.o, $(filter %.S,$(COMPONENT_SRCS)))
+COMPONENT_SRCDIRS := .
+
+ifeq ($(CONFIG_CHIP_NAME),BL602)
+CFLAGS += -DBL602
+else ifeq ($(CONFIG_CHIP_NAME),BL702)
+CFLAGS += -DBL702
+else ifeq ($(CONFIG_CHIP_NAME),BL702L)
+CFLAGS += -DBL702L
+else ifeq ($(CONFIG_CHIP_NAME),BL616)
+CFLAGS += -DBL616
+endif
